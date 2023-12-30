838d0c8acd7836d5f7cb0508392a758c81d4ff69
Gallery <59381987+Gallerys@users.noreply.github.com>
2023-12-30 11:49:08 +0600

    Update MainActivity.java

--- app/src/main/java/com/example/loginauthenticator/MainActivity.java
+++ app/src/main/java/com/example/loginauthenticator/MainActivity.java
@@ -1,8 +1,6 @@
 package com.example.loginauthenticator;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Patterns;
@@ -10,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
